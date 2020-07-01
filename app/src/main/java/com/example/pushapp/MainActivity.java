f1109db77da07147d0a5244d6404e079b716f369
arunsri7 <arun.i1997@gmail.com>
2020-07-01 10:35:48 +0530

    Added bottom navigation

--- app/src/main/java/com/example/pushapp/MainActivity.java
+++ app/src/main/java/com/example/pushapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.pushapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -13,39 +18,47 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-    TextView countTv;
-    RelativeLayout increment;
-    Button reset;
-    int count = 0;
+public class MainActivity extends AppCompatActivity {
+    private ActionBar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Bottom Navigation
 
-        //Pull to Refresh
-        final SwipeRefreshLayout pullToRefresh = findViewById(R.id.pullToRefresh);
-        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                int refreshedValue = 0;
-                countTv.setText(String.valueOf(refreshedValue));
-                pullToRefresh.setRefreshing(false);
+        toolbar = getSupportActionBar();
+        BottomNavigationView navigation = findViewById(R.id.navigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        toolbar.setTitle("PushApp");
+        loadFragment(new PushappFragment());
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.pushapp:
+                    toolbar.setTitle("Pushapp");
+                    loadFragment(new PushappFragment());
+                    return true;
+                case R.id.records:
+                    toolbar.setTitle("Records");
+                    loadFragment(new RecordsFragment());
+                    return true;
             }
-        });
+            return false;
+        }
+    };
 
-        //Initialization.
-        countTv = findViewById(R.id.count_tv);
-        increment = findViewById(R.id.increment);
-
-        increment.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view)
-            {
-                count++;//increment the count
-                countTv.setText(String.valueOf(count));// view in the text
-            }
-
-        });
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/pushapp/PushappFragment.java
@@ -0,0 +1,74 @@
+package com.example.pushapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+public class PushappFragment extends Fragment {
+
+    TextView countTv;
+    FrameLayout increment;
+    Button reset;
+    int count = 0;
+
+    public PushappFragment() {
+        // Required empty public constructor
+    }
+
+    public static PushappFragment newInstance(String param1, String param2) {
+        PushappFragment fragment = new PushappFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pushapp, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+                //Pull to Refresh
+        final SwipeRefreshLayout pullToRefresh = view.findViewById(R.id.pullToRefresh);
+        pullToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                count = 0;
+                countTv.setText(String.valueOf(count));
+                pullToRefresh.setRefreshing(false);
+            }
+        });
+
+        //Initialization.
+        countTv = view.findViewById(R.id.count_tv);
+        increment = view.findViewById(R.id.increment);
+
+        increment.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {
+                count++;//increment the count
+                countTv.setText(String.valueOf(count));// view in the text
+            }
+
+        });
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
