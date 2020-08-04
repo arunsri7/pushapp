87c473dd61bed04e7a39789a9cde19d9236af3f1
arunsri7 <arun.i1997@gmail.com>
2020-08-04 20:30:41 +0530

    [implemented Android Onboarding screen]

---
+++ app/src/main/java/com/example/pushapp/OnBoardingActivity.java
@@ -0,0 +1,59 @@
+package com.example.pushapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.ramotion.paperonboarding.PaperOnboardingEngine;
+import com.ramotion.paperonboarding.PaperOnboardingPage;
+import com.ramotion.paperonboarding.listeners.PaperOnboardingOnChangeListener;
+import com.ramotion.paperonboarding.listeners.PaperOnboardingOnRightOutListener;
+
+import java.util.ArrayList;
+
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onboarding_main_layout);
+
+        PaperOnboardingEngine engine = new PaperOnboardingEngine(findViewById(R.id.onboardingRootView), getDataForOnboarding(), getApplicationContext());
+
+        engine.setOnChangeListener(new PaperOnboardingOnChangeListener() {
+            @Override
+            public void onPageChanged(int oldElementIndex, int newElementIndex) {
+                //
+            }
+        });
+
+        engine.setOnRightOutListener(new PaperOnboardingOnRightOutListener() {
+            @Override
+            public void onRightOut() {
+                // Probably here will be the exit action
+                Intent i = new Intent(OnBoardingActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+    // Just example data for Onboarding
+    private ArrayList<PaperOnboardingPage> getDataForOnboarding() {
+        // prepare data
+        PaperOnboardingPage scr1 = new PaperOnboardingPage("PushApp", "Do push ups the right way",
+                Color.parseColor("#678FB4"), R.drawable.avengers, R.drawable.skip_icon);
+        PaperOnboardingPage src2 = new PaperOnboardingPage("Demo", "Place your phone on the floor and touch your nose to the screen as you do a push up. As simple as that!",
+                Color.parseColor("#9B90BC"), R.drawable.avengers, R.drawable.skip_icon);
+
+        ArrayList<PaperOnboardingPage> elements = new ArrayList<>();
+        elements.add(scr1);
+        elements.add(src2);
+        return elements;
+    }
+}
